d9c93f14fc7186531414caaa68d9653d042f8fe1
Swapnil-DevGeek <f20220181@goa.bits-pilani.ac.in>
2024-12-22 23:15:33 +0530

    complete project routes

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -2,7 +2,7 @@ const asyncHandler =require('express-async-handler');
 const Project = require('../models/Project.model');
 
 // @desc    Create new project
-// @route   POST /api/projects
+// @route   POST /api/projects/
 // @access  Private(admin only)
 const createProject = asyncHandler(async (req, res) => {
     const {project_name} = req.body;
@@ -38,4 +38,68 @@ const createProject = asyncHandler(async (req, res) => {
 
 });
 
-module.exports = {createProject};
+// @desc    Get all projects
+// @route   GET /api/projects
+// @access  Private
+const getAllProjects = asyncHandler(async (req, res) => {
+    const projects = await Project.find({});
+    return res.status(200).json(projects);
+});
+
+// @desc    Get project by ID
+// @route   GET /api/projects/:id
+// @access  Private
+const getProjectById = asyncHandler(async (req, res) => {
+    const project = await Project.findById(req.params.id);
+    if(project){
+        return res.status(200).json(project);
+    } else {
+        return res.status(404).json({message:'Project not found'});
+    }
+});
+
+// @desc Delete project
+// @route DELETE /api/projects/:id
+// @access Private (admin only)
+const deleteProject = asyncHandler(async (req, res) => {
+    const project = await Project.findById(req.params.id);
+    if(req.user.designation !== 'L3'){
+        return res.status(401).json({message: 'Not authorized as an admin'}) ;
+    }
+    if(project){
+        await Project.deleteOne({_id: req.params.id});
+        return res.json({message: 'Project removed'});
+    } else {
+        return res.status(404).json({message: 'Project not found'});
+    }
+});
+
+// @desc Update project
+// @route PUT /api/projects/:id
+// @access Private (admin only)
+const updateProject = asyncHandler(async (req, res) => {
+    const project = await Project.findById(req.params.id);
+    if(req.user.designation !== 'L3'){
+        return res.status(401).json({message: 'Not authorized as an admin'}) ;
+    }
+    if(project){
+        project.project_name = req.body.project_name || project.project_name;
+        const updatedProject = await project.save();
+        return res.json({
+            _id: updatedProject._id,
+            project_name: updatedProject.project_name,
+            created_by: updatedProject.created_by,
+            created_at: updatedProject.created_at,
+        });
+    } else {
+        return res.status(404).json({message: 'Project not found'});
+    }
+});
+
+module.exports = {
+    createProject,
+    getAllProjects,
+    getProjectById,
+    deleteProject,
+    updateProject
+};
--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const { createProject } = require('../controllers/projectController');
+const { createProject, getAllProjects, getProjectById, updateProject, deleteProject } = require('../controllers/projectController');
 const { protect } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
 // POST /api/projects - Create a new project (Admin only)
 router.post('/', protect, createProject);
+router.get('/',protect,getAllProjects);
+router.get('/:id',protect,getProjectById);
+router.put('/:id',protect,updateProject);
+router.delete('/:id',protect,deleteProject);
 
 module.exports = router;
